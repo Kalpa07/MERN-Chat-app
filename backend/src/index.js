7b70ae577bc54d0d7a6dd27d285e08b57b2591f4
Kalpa07 <kalpateli0711@gmail.com>
2025-03-25 00:06:48 +0530

    [fixed deployment issue]

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 
 import path from "path"; //module built in node
+// import { fileURLToPath } from "url";
 
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
@@ -16,6 +17,8 @@ dotenv.config();
 
 const PORT = process.env.PORT;
 const __dirname = path.resolve();//
+
+
 
 // app.use(express.json()); //(parse json) help to extract json data out of body in controllers
 app.use(express.json({ limit: "10mb" })); 
@@ -33,18 +36,16 @@ app.use("/api/messages",messageRoutes);
 //if we are in production make dist folder our static assets
 if(process.env.Node_ENV ==="production"){
     app.use(express.static(path.join(__dirname, "../frontend/dist"))); //static is a built in middleware
+    // console.log("__dirname : " + __dirname);
 
     //this is for routes, 
     // if we go for any other routes than these two, then we will call the frontend routes
     // app.use("/api/auth",authRoutes);
     //app.use("/api/messages",messageRoutes);
-    app.get("*", (res,re)=>{
+    app.get("*", (req,res)=>{
         res.sendFile(path.join(___dirname,"../frontend","dist","index.html")); 
     });
-
-
     //
-
 }
 
 // app.listen(PORT, ()=>{
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
     // baseURL: "http://localhost:5001/api", // this is for development
-    baseURL: import.meta.env.MODE ==="development"? "http://localhost:5001/api" : "api", 
+    baseURL: import.meta.env.MODE ==="development"? "http://localhost:5001/api" : "/api", 
     withCredentials:true, //helps in sending the cookie in each and every request
 });
